e589bbfdd5cb2cb10bf69eb89a89581f51a0e17a
Kristofer Joseph <kristofer@kristoferjoseph.com>
2010-07-26 20:04:05 -0700

    adding as3 property to_s and tests

--- lib/as3_property.rb
+++ lib/as3_property.rb
@@ -5,4 +5,23 @@ class As3Property
   attr_accessor :name
   attr_accessor :value
   attr_accessor :type
+  
+  def get_static
+    " static" unless !@is_static
+  end
+  
+  def get_value
+    unless @value.nil?
+      if @type == "String"
+        "=\"""#{@value}""\""
+      else
+        "=#{@value}"
+      end
+    end
+  end
+  
+  def to_s
+    "#{@modifier}#{get_static} var #{@name}:#{@type}#{get_value}"
+  end
+  
 end
---
+++ test/as3_property_test.rb
@@ -0,0 +1,39 @@
+require File.join(File.dirname(__FILE__), "test_helper")
+
+class As3PropertyTest < Test::Unit::TestCase
+
+  context "An AS3 property" do
+    
+    should "render to string" do
+      prop = As3Property.new
+      prop.modifier = "public"
+      prop.is_static = true
+      prop.name = "fwee"
+      prop.value = "fugnucket"
+      prop.type = "String"
+      assert_equal 'public static var fwee:String="fugnucket"', prop.to_s
+    end
+    
+    should "render to string with numbers" do
+      prop = As3Property.new
+      prop.modifier = "protected"
+      prop.is_static = false
+      prop.name = "fwee"
+      prop.value = "5"
+      prop.type = "int"
+      assert_equal 'protected var fwee:int=5', prop.to_s
+    end
+    
+    should "render to string with Classes" do
+      prop = As3Property.new
+      prop.modifier = "private"
+      prop.is_static = false
+      prop.name = "fwi"
+      prop.value = "DisplayObject"
+      prop.type = "Class"
+      assert_equal 'private var fwi:Class=DisplayObject', prop.to_s
+    end
+    
+  end
+  
+end
