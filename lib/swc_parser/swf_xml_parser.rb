8dc185e8bc74a2b03750e9fbf7130667444ddd04
Kristofer Joseph <kristofer@kristoferjoseph.com>
2010-07-28 22:07:48 -0700

    Skipping parsing of excluded classes

--- lib/swc_parser/swf_xml_parser.rb
+++ lib/swc_parser/swf_xml_parser.rb
@@ -38,131 +38,130 @@ module SWCParser
       doc = REXML::Document.new(tag.to_s)
       root = doc.root
       as3_data = As3ClassData.new()
-
-      # # Skip excluded classes
-      # metadata_nodes = doc.get_elements('//MetaDataNode')
-      # unless metadata_nodes.empty?
-      #   metadata_nodes.each do |data|
-      #     if data.has_attributes?
-      #       meta_attrs = data.attributes
-      #       meta_attrs.each do |a|
-      #         p "attr #{a}"
-      #         if a["id"] == "ExcludeClass"
-      #           return
-      #         end
-      #       end
-      #     end
-      #   end
-      # end
-
-
-      # Parse the package and class name
-      as3_data.fully_qualified_class_name = root.attributes["name"]
-
-      # Parse the super class name
-      stringNode = doc.get_elements('//baseclass/LiteralStringNode')
-      if stringNode[0]
-        as3_data.fully_qualified_super_class_name = stringNode[0].attributes["value"]
-      end
-
-      # See if we are dealing with an interface class
-      interface_definition_node =  doc.get_elements('//BinaryInterfaceDefinitionNode')
-      if interface_definition_node[0]
-        as3_data.is_interface = interface_definition_node[0].has_attributes?
-      end
-
-      # Parse the class modifier
-      if as3_data.is_interface
-        modifier = get_modifier interface_definition_node[0].elements['attrs/AttributeListNode'].attributes
-      else
-        modifier = get_modifier doc.get_elements('//BinaryClassDefNode')[0].elements['attrs/AttributeListNode'].attributes
-      end
-
-      # Parse the interfaces
-      interfaces = doc.get_elements('//interfaces//IdentifierNode')
-      unless interfaces.empty?
-        interfaces.each do |e|
-          as3_data.interfaces << e.attributes["name"]
-        end
-      end
-
-      # Parse the public properties
-      properties = doc.get_elements('//VariableBindingNode')
-      unless properties.empty?
-        properties.each do |property|
-          prop = As3Property.new
-          qualifier_node = property.elements['TypedIdentifierNode/QualifiedIdentifierNode']
-          prop.name = qualifier_node.attributes["name"]
-          attribute_array = qualifier_node.elements['AttributeListNode'].attributes
-          prop.modifier = get_modifier attribute_array
-
-          if attribute_array["static"]
-            prop.is_static = true
-          else
-            prop.is_static = false
-          end
-
-          value_node = property.elements['LiteralStringNode']
-          if value_node
-            prop.value = value_node.attributes['value']
-          end
-
-          type_node = property.elements['TypedIdentifierNode/MemberExpressionNode/selector/GetExpressionNode/IdentifierNode']
-          if type_node
-            prop.type = type_node.attributes['name']
-          end
-
-          as3_data.properties << prop
-        end
-      end
-
-      # Parse all the methods
-      methods = doc.get_elements('//FunctionCommonNode')
-      unless methods.empty?
-        methods.each do |method|
-          function = As3Method.new
-
-          # Parse method name
-          function.name = method.elements['QualifiedIdentifierNode'].attributes['name']
-
-          # Parse method modifier
-          modifier_attributes = method.elements['QualifiedIdentifierNode/AttributeListNode'].attributes
-          function.modifier = get_modifier modifier_attributes
-
-          # Parse if method is static
-          if modifier_attributes["static"]
-            function.is_static = true
-          end
-
-          # Parse function parameters
-          parameters = method.get_elements('//ParameterNode')
-          unless parameters.empty?
-            parameters.each do |parameter|
-              param = As3MethodParam.new
-              param.name = parameter.elements['identifier/IdentifierNode'].attributes['name']
-              param.type = parameter.elements['type/MemberExpressionNode/selector/GetExpressionNode/IdentifierNode'].attributes['name']
-              default = parameter.elements['init/LiteralStringNode']
-              if default && default.has_attributes?
-                param.default = default.attributes['value']
-              end
-
-              function.parameters << param
+      
+      exclude_class = false
+      
+      # Skip excluded classes
+      metadata_nodes = doc.get_elements('//MetaDataNode')
+      unless metadata_nodes.empty?
+        metadata_nodes.each do |node|
+          if node.has_attributes?
+            if node.attributes["id"] == "ExcludeClass"
+              exclude_class = true
             end
           end
-
-          # Parse function return type
-          return_type_node = method.elements['FunctionSignatureNode/MemberExpressionNode/selector/GetExpressionNode/IdentifierNode']
-          # We have to do this nil check here because constructors have no return type
-          if return_type_node && return_type_node.has_attributes?
-            function.return_type =  return_type_node.attributes['name']
-          end
-
-          as3_data.functions << function
         end
       end
 
-      @as3_classes << as3_data
-      write_out_intrinsic_class(as3_data)
+      unless exclude_class
+        # Parse the package and class name
+        as3_data.fully_qualified_class_name = root.attributes["name"]
+
+        # Parse the super class name
+        stringNode = doc.get_elements('//baseclass/LiteralStringNode')
+        if stringNode[0]
+          as3_data.fully_qualified_super_class_name = stringNode[0].attributes["value"]
+        end
+
+        # See if we are dealing with an interface class
+        interface_definition_node =  doc.get_elements('//BinaryInterfaceDefinitionNode')
+        if interface_definition_node[0]
+          as3_data.is_interface = interface_definition_node[0].has_attributes?
+        end
+
+        # Parse the class modifier
+        if as3_data.is_interface
+          modifier = get_modifier interface_definition_node[0].elements['attrs/AttributeListNode'].attributes
+        else
+          modifier = get_modifier doc.get_elements('//BinaryClassDefNode')[0].elements['attrs/AttributeListNode'].attributes
+        end
+
+        # Parse the interfaces
+        interfaces = doc.get_elements('//interfaces//IdentifierNode')
+        unless interfaces.empty?
+          interfaces.each do |e|
+            as3_data.interfaces << e.attributes["name"]
+          end
+        end
+
+        # Parse the public properties
+        properties = doc.get_elements('//VariableBindingNode')
+        unless properties.empty?
+          properties.each do |property|
+            prop = As3Property.new
+            qualifier_node = property.elements['TypedIdentifierNode/QualifiedIdentifierNode']
+            prop.name = qualifier_node.attributes["name"]
+            attribute_array = qualifier_node.elements['AttributeListNode'].attributes
+            prop.modifier = get_modifier attribute_array
+
+            if attribute_array["static"]
+              prop.is_static = true
+            else
+              prop.is_static = false
+            end
+
+            value_node = property.elements['LiteralStringNode']
+            if value_node
+              prop.value = value_node.attributes['value']
+            end
+
+            type_node = property.elements['TypedIdentifierNode/MemberExpressionNode/selector/GetExpressionNode/IdentifierNode']
+            if type_node
+              prop.type = type_node.attributes['name']
+            end
+
+            as3_data.properties << prop
+          end
+        end
+
+        # Parse all the methods
+        methods = doc.get_elements('//FunctionCommonNode')
+        unless methods.empty?
+          methods.each do |method|
+            function = As3Method.new
+
+            # Parse method name
+            function.name = method.elements['QualifiedIdentifierNode'].attributes['name']
+
+            # Parse method modifier
+            modifier_attributes = method.elements['QualifiedIdentifierNode/AttributeListNode'].attributes
+            function.modifier = get_modifier modifier_attributes
+
+            # Parse if method is static
+            if modifier_attributes["static"]
+              function.is_static = true
+            end
+
+            # Parse function parameters
+            parameters = method.get_elements('//ParameterNode')
+            unless parameters.empty?
+              parameters.each do |parameter|
+                param = As3MethodParam.new
+                param.name = parameter.elements['identifier/IdentifierNode'].attributes['name']
+                param.type = parameter.elements['type/MemberExpressionNode/selector/GetExpressionNode/IdentifierNode'].attributes['name']
+                default = parameter.elements['init/LiteralStringNode']
+                if default && default.has_attributes?
+                  param.default = default.attributes['value']
+                end
+
+                function.parameters << param
+              end
+            end
+
+            # Parse function return type
+            return_type_node = method.elements['FunctionSignatureNode/MemberExpressionNode/selector/GetExpressionNode/IdentifierNode']
+            # We have to do this nil check here because constructors have no return type
+            if return_type_node && return_type_node.has_attributes?
+              function.return_type =  return_type_node.attributes['name']
+            end
+
+            as3_data.functions << function
+          end
+        end
+
+        @as3_classes << as3_data
+        write_out_intrinsic_class(as3_data)
+      end
     end
 
     def get_modifier(array)
